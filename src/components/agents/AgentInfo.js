788d78627ebd9aea2669837bfa1c667a96de70d6
Mohit-prog <m98716@gmail.com>
2023-01-31 03:31:44 +0530

    infovideos

--- src/components/agents/AgentInfo.js
+++ src/components/agents/AgentInfo.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ShimmerUI from "../common/ShimmerUI";
+import agentVideoObject from '../agents/agentvideoBG/agentVideoObject'
 const AgentInfo = () => {
   const { name } = useParams();
   const [agent, setAgent] = useState();
@@ -15,13 +16,19 @@ const AgentInfo = () => {
     setAgent(json?.data);
   }
 
+
   return !agent ? (
     <ShimmerUI />
   ) : (
     <>
       <div className="agentInfo">
+      
         <h1 className="agentName">{agent?.displayName}</h1>
-        <img className="agentLargeImage" src={agent?.fullPortrait} />
+       
+        <div className=" agentvideobg"> 
+      <video src={agentVideoObject[agent.displayName]} autoPlay loop muted />
+   
+    </div>
         <h1 className="agentDescription">{agent?.description}</h1>
         <div className="abilities">
           {agent.abilities.map((ability) => (
---
+++ src/components/agents/agentvideoBG/agentVideoObject.js
@@ -0,0 +1,17 @@
+import sage from "./sage.mp4";
+import reyna from "./reyna.mp4";
+import neon from "./neon.mp4";
+import sova from "./sova.mp4";
+import fade from "./fade.mp4";
+import astra from "./astra.mp4";
+  
+  const agentVideoObject ={
+    Sage :sage,
+    Reyna:reyna,
+    Neon:neon,
+    Sova:sova,
+    Fade:fade,
+    Astra:astra
+
+}
+export default agentVideoObject;
